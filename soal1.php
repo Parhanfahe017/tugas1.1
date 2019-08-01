5354df3d77683df04364f45da3097eac32458726
Lab RPL <parhanfahe@gmail.com>
2019-08-01 16:46:19 +0700

    ini projoct soal pertama

---
+++ soal1.php
@@ -0,0 +1,10 @@
+<?php
+$jambu    = 15000;
+$dus      = 5*6;
+$jumh_dus = 2000*6;
+$total    = $jambu*$dus;
+$total1   = ($jambu*$dus)+$jumh_dusjumh;
+
+echo "harga jual jambu $total <br>";
+echo "harga jual jambu 2 $total1 <br>";
+?>
